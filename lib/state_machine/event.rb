035b8174f4970cef2449c6669be3aea2c7025bfc
Piotr Niełacny <piotr.nielacny@gmail.com>
2016-07-26 23:10:03 +0200

    Add transitions to implementation
    User can define events and transitions types in state machine. We are
    sure that we can not jump around from any state to another. User can
    define restrictions here.

--- lib/state_machine/event.rb
+++ lib/state_machine/event.rb
@@ -1,15 +1,47 @@
 module StateMachine
   module Event
+    InvalidDefinition    = Class.new(StandardError)
+    UnexpectedTransition = Class.new(StandardError)
+
     module ClassMethods
-      def event(name)
-        define_event_method(name)
+      attr_accessor :possible_transitions, :name
+
+      def event(name, &block)
+        self.name = name
+        self.possible_transitions ||= {}
+        self.possible_transitions[name] = {}
+
+        if block_given?
+          to = instance_eval(&block)
+          return define_event_method(name, to)
+        end
+
+        raise InvalidDefinition
+      end
+
+      def transitions(options = {})
+        from = Array(options[:from])
+        to   = options[:to]
+
+        raise InvalidDefinition if from.empty? && to.nil?
+
+        possible_transitions[name][to] ||= []
+        possible_transitions[name][to] += from
+        possible_transitions[name][to].uniq!
+
+        to
       end
 
     private
 
-      def define_event_method(name)
-        define_method("#{name}!") do
+      def define_event_method(name, to)
+        define_method("can_#{name}?") do
+          self.class.possible_transitions[name][to].include?(state)
+        end
 
+        define_method("#{name}!") do
+          return self.state = to if public_send("can_#{name}?")
+          raise UnexpectedTransition
         end
       end
     end
--- spec/state_machine/event_spec.rb
+++ spec/state_machine/event_spec.rb
@@ -4,14 +4,94 @@ RSpec.describe StateMachine::Event do
   let(:test_class) { Class.new { include StateMachine } }
 
   context ".event" do
-    before { test_class.state :standing, initial: true }
+    before do
+      test_class.state :standing, initial: true
+      test_class.state :walking
+      test_class.state :running
+    end
 
     subject { test_class.new }
 
-    it "defines event" do
-      test_class.event :walk
+    context "invalid definition" do
+      context "no block" do
+        it "raises an exception" do
+          expect { test_class.event :walk }.to raise_error(StateMachine::Event::InvalidDefinition)
+        end
+      end
 
-      expect { subject.walk! }.not_to raise_error(NoMethodError)
+      context "invalid paramters" do
+        it "raises an exception" do
+          expect do
+            test_class.event :walk do
+              transitions from: []
+            end
+          end.to raise_error(StateMachine::Event::InvalidDefinition)
+        end
+      end
+    end
+
+    context "definition with transition" do
+      it "change state to value given in `:to` key" do
+        test_class.event :walk do
+          transitions from: :standing, to: :walking
+        end
+
+        subject.walk!
+        expect(subject.walking?).to be_truthy
+      end
+
+      it "does not allow to unallowed transition" do
+        test_class.event :run do
+          transitions from: :walking, to: :running
+        end
+
+        expect { subject.run! }.to raise_error(StateMachine::Event::UnexpectedTransition)
+      end
+
+      it "allows to define many `from`s" do
+        test_class.event :sonic do
+          transitions from: [:standing, :running], to: :running
+        end
+
+        subject.sonic!
+        expect(subject.running?).to be_truthy
+      end
+    end
+  end
+
+  context ".can_*?" do
+    subject { test_class.new }
+
+    before do
+      test_class.state :standing, initial: true
+      test_class.state :walking
+      test_class.state :running
+
+      test_class.event :walk do
+        transitions from: :standing, to: :walking
+      end
+
+      test_class.event :run do
+        transitions from: :walking, to: :running
+      end
+
+      test_class.event :sonic do
+        transitions from: [:standing, :running], to: :running
+      end
+    end
+
+    context "#can_{transition}?" do
+      context "allowed transition" do
+        it "returns true" do
+          expect(subject.can_walk?).to be_truthy
+        end
+      end
+
+      context "unallowed transition" do
+        it "returns false" do
+          expect(subject.can_run?).to be_falsey
+        end
+      end
     end
   end
 end
